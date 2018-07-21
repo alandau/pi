7c881cbcbda69d2f6835afd60d785439d9bfea24
Alex Landau <landau.alex@gmail.com>
2018-07-21 13:21:44 -0700

    Added "Get Direct URL"

--- website/app.py
+++ website/app.py
@@ -10,17 +10,25 @@ def main():
     url = request.args.get('url', '')
     if url == 'about:blank':
         url = ''
-    done = request.args.get('done', False)
-    return render_template('index.html', url=url, base=request.base_url, done=done)
+    done = request.args.get('done', None)
+    direct_url = request.args.get('direct_url', None)
+    return render_template('index.html', url=url, base=request.base_url, done=done, direct_url=direct_url)
 
 @app.route("/action")
 def action():
     url = request.args.get('url', '')
+    direct_url = None
+    done = True
     if request.args.get('open', ''):
         subprocess.Popen("DISPLAY=:0 /home/pi/player/player.py '%s'" % url, shell=True)
     elif request.args.get('browser', ''):
         subprocess.Popen("DISPLAY=:0 xdg-open '%s'" % url, shell=True)
-    return redirect(url_for('main', url=url, done=True))
+    elif request.args.get('yt', ''):
+        try:
+            direct_url = subprocess.check_output('youtube-dl -f "best[height<=?720][ext=mp4]" -g --no-playlist -- "%s" 2>&1' % url, shell=True).strip()
+        except subprocess.CalledProcessError as e:
+            done = False
+    return redirect(url_for('main', url=url, done=done, direct_url=direct_url))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
