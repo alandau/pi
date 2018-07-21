2280a9526aa2d8c399c7b8bb13d3f9ff7e235aa5
Alex Landau <landau.alex@gmail.com>
2018-07-21 14:27:13 -0700

    View playlists and videos through website

--- website/app.py
+++ website/app.py
@@ -2,8 +2,17 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 import subprocess
+import os
+
+PLAYLIST_DIR = '/home/pi/playlists'
 
 app = Flask(__name__)
+
+def get_yt_url(url):
+    try:
+        return subprocess.check_output('youtube-dl -f "best[height<=?720][ext=mp4]" -g --no-playlist -- "%s" 2>&1' % url, shell=True).strip()
+    except subprocess.CalledProcessError as e:
+        return None
 
 @app.route("/")
 def main():
@@ -24,11 +33,58 @@ def action():
     elif request.args.get('browser', ''):
         subprocess.Popen("DISPLAY=:0 xdg-open '%s'" % url, shell=True)
     elif request.args.get('yt', ''):
-        try:
-            direct_url = subprocess.check_output('youtube-dl -f "best[height<=?720][ext=mp4]" -g --no-playlist -- "%s" 2>&1' % url, shell=True).strip()
-        except subprocess.CalledProcessError as e:
+        direct_url = get_yt_url(url)
+        if not direct_url:
             done = False
     return redirect(url_for('main', url=url, done=done, direct_url=direct_url))
+
+def load_playlist(filename):
+    filename = os.path.join(PLAYLIST_DIR, filename)
+    with open(filename) as f:
+        lines = f.readlines()
+    data = []
+    if not lines:
+        return data
+    if lines[0].strip() != '#EXTM3U':
+        for l in lines:
+            url = l.strip()
+            data.append(dict(url=url, title=url))
+        return data
+    i = 1
+    title = None
+    while i < len(lines):
+        line = lines[i].strip()
+        i += 1
+        if line.startswith('#EXTINF:'):
+            split = line[8:].split(',', 1)
+            title = split[0] if len(split) == 1 else split[1]
+            continue
+        url = line
+        if not title:
+            title = url
+        data.append(dict(url=url, title=title))
+        title = None
+    return data
+
+@app.route("/playlist")
+def playlist():
+    name = request.args.get('name')
+    if not name:
+        playlists = [[(f[:-4].decode('utf-8'),url_for('playlist', name=f))] for f in sorted(os.listdir(PLAYLIST_DIR)) if f.endswith('.m3u')]
+        return render_template('table.html', columns=['Playlist'], rows=playlists)
+    data = load_playlist(name)
+    playlist = [[('Direct', item['url']), ('Youtube', url_for('getyt', url=item['url'])), (item['title'].decode('utf-8'), None)] for item in data]
+    return render_template('table.html', columns=['Direct', 'Youtube', 'Title'], rows=playlist)
+
+@app.route("/getyt")
+def getyt():
+    url = request.args.get('url')
+    if not url:
+        return 'Empty url'
+    url = get_yt_url(url)
+    if not url:
+        return "Can't get video url"
+    return redirect(url)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
